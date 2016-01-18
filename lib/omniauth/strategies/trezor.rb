75b4a6d917e1e835c25b4841a99b1b27c3a58dcf
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2016-01-18 14:28:22 +0100

    Fixes setting of default values

--- lib/omniauth/strategies/trezor.rb
+++ lib/omniauth/strategies/trezor.rb
@@ -7,15 +7,21 @@ module OmniAuth
     class Trezor
       include OmniAuth::Strategy
 
-      option :visual_challenge, Time.now.strftime("%Y-%m-%d %H:%M:%S")
-      option :hidden_challenge, SecureRandom.hex(32)
+      option :visual_challenge
+      option :hidden_challenge
       option :hosticon
       option :fields, [:public_key, :signature]
       option :uid_field, :public_key
 
       def request_phase
-        session['omniauth.trezor_visual_challenge'] = options[:visual_challenge]
-        session['omniauth.trezor_hidden_challenge'] = options[:hidden_challenge]
+        visual_challenge = options[:challenge_visual]
+        visual_challenge ||= Time.now.strftime("%Y-%m-%d %H:%M:%S")
+
+        hidden_challenge = options[:hidden_challenge]
+        hidden_challenge ||= SecureRandom.hex(32)
+
+        session['omniauth.trezor_visual_challenge'] = visual_challenge
+        session['omniauth.trezor_hidden_challenge'] = hidden_challenge
 
         OmniAuth::Form.build(
           title: "Trezor Login",
@@ -25,7 +31,7 @@ module OmniAuth
             <script src="https://trezor.github.io/connect/login.js"></script>
             <script type='text/javascript'>
               function trezorLogin() {
-                TrezorConnect.requestLogin('#{options[:hosticon]}', '#{options[:hidden_challenge]}', '#{options[:visual_challenge]}', function (result) {
+                TrezorConnect.requestLogin('#{options[:hosticon]}', '#{hidden_challenge}', '#{visual_challenge}', function (result) {
                     if (result.success) {
                         $('input[name=public_key]').val(result.public_key);
                         $('input[name=signature]').val(result.signature);
@@ -47,7 +53,7 @@ module OmniAuth
         ) do |f|
           f.input_field('hidden', 'public_key')
           f.input_field('hidden', 'signature')
-          f.html "<p>Logging in at: #{options[:visual_challenge]}</p>"
+          f.html "<p>Logging in at: #{visual_challenge}</p>"
         end.to_response
       end
 
